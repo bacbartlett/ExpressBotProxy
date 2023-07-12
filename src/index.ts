5a20f524649d0c749919847c4d135d1c54df647c
Brandon Bartlett <brandonacb@gmail.com>
2023-07-11 23:14:01 -0400

    Proxy Code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import path from 'path';
+import fetch from 'node-fetch';
 import examplerouter from './routes/examplerouter';
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +11,22 @@ app.use(express.static(pathToPublic))
 
 app.use('/router', examplerouter)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/standard/:url*', async (req: Request, res: Response) => {
+  const url = req.params.url + req.params[0]
+  const data = await (await fetch(url)).text()
+  res.send(data);
+});
+
+app.get('/bot/:url*', async (req: Request, res: Response) => {
+  const url = req.params.url + req.params[0]
+  const data = await (await fetch(url, {headers: {'User-Agent': `Mozilla/5.0 (compatible; Twitterbot/1.0)`}})).text()
+  res.send(data);
+});
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello world');
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
